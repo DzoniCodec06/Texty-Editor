9323059c90115d54a6d0035b63cffe6f2ae8b6ae
Nikola Bojinović <bojinovicnikola12@gmail.com>
2023-08-31 23:56:32 +0200

    Add files via upload

--- Texty-Editor/src/app.js
+++ Texty-Editor/src/app.js
@@ -1,9 +1,15 @@
 const scriptInput = document.getElementById("script");
 const rowsIndentifier = document.getElementById("rows-indentifier");
 
+const saveButton = document.getElementById("save-btn");
+
+const openFile = document.getElementById("fileOpen");
+
+const fileInput = document.getElementById("fileInput");
+
 let n = 1;
 
-let text = [];
+let text;
 
 function addNewRow() {
     const newRow = document.createElement("p");
@@ -20,28 +26,49 @@ function removeLastRow() {
         rowsIndentifier.removeChild(rowsIndentifier.lastChild);
         n--;
         scriptInput.rows = n - 1;
-    } 
+    }
 }
 
-addNewRow();
+scriptInput.addEventListener("change", () => {
+    text = scriptInput.value.split("\n");
+})
 
 
 document.addEventListener("keydown", e => {
     if (e.key == "Enter") addNewRow();
     else if (e.key == "Backspace" && e.data == null) removeLastRow();
     console.log(scriptInput.rows);
-});
+}); 
 
-
-/*
-scriptInput.addEventListener("input", e => {
-    if (e.key == "Enter") addNewRow();
-    else if (e.key == "Backspace" && e.data == null) removeLastRow();
+saveButton.addEventListener("click", () => {
+    const blob = new Blob([scriptInput.value], { type: "text/plain" });
+    const a = document.createElement("a");
+    a.href = URL.createObjectURL(blob);
+    a.download = "textFile.txt";
+    a.click();
 })
-*/
 
+openFile.addEventListener("click", () => {
+    fileInput.click();
+})
 
-/*
-console.log(scriptInput.cols);
-console.log(scriptInput.rows);
-*/
+fileInput.addEventListener("change", (event) => {
+    const file = event.target.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+            
+        reader.onload = function(e) {
+            const fileContent = e.target.result;
+            scriptInput.value = fileContent;
+
+            const lines = fileContent.split("\n");
+
+            for (let i = 1; i < lines.length; i++) addNewRow();
+        };
+
+        reader.readAsText(file);
+    }
+})
+
+addNewRow();
